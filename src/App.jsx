5990b99312361c52fe04976e6717f6932d0729df
Mısra Medenioğlu <misramedenioglu0@gmail.com>
2024-11-01 17:27:41 +0300

    fix blank answer bug

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,20 @@ const questions = [
     question: "Çin Seddini oluşturan taşlar birbirine ne ile tutturulmuştur?",
     options: ["Bambu Harcı", "Anne Duası", "Pirinç Unu", "Noodle"],
     answer: "Pirinç Unu",
-    media: "/assets/cin-seddi.jpg",
+    media: "/src/assets/cin-seddi.jpg",
   },
   {
     question: "İlk Pamuk şekeri bulan kişinin mesleği nedir?",
     options: ["Gıda Mühendisi", "Diş Doktoru", "Ev Hanımı", "Güzellik Uzmanı"],
     answer: "Diş Doktoru",
-    media: "/assets/pamuk.jpg",
+    media: "/src/assets/pamuk.jpg",
   },
   {
     question:
       "Tarkan'ın 'Hüp' klibini izledikten sonra gaza gelip 'Tarkan keşke beni hüpletseydi' diye açıklamda bulunan kişi kimdir?",
     options: ["Gülben Ergen", "Hülya Avşar", "Harika Avcı", "Sevtap Parman"],
     answer: "Gülben Ergen",
-    media: "/assets/tarkan.jpg",
+    media: "/src/assets/tarkan.jpg",
   },
   {
     question: "Pteronofobi nedir?",
@@ -32,47 +32,47 @@ const questions = [
       "Kuş tüyüyle gıdıklanma korkusu",
     ],
     answer: "Kuş tüyüyle gıdıklanma korkusu",
-    media: "/assets/fobi.jpg",
+    media: "/src/assets/fobi.jpg",
   },
   {
     question:
       "Ortalama ömürleri 5 yıl olan Japon balıklarının en uzun yaşayanı Tish, bütün istatistikleri alt üst ederek kaç yıl boyunca hayata tutunmayı başarmıştır?",
     options: ["43", "78", "23", "99"],
     answer: "43",
-    media: "/assets/balik.jpg",
+    media: "/src/assets/balik.jpg",
   },
   {
     question:
       "90'lara damgasını vuran 'Bandıra Bandıra' şarkısının söz yazarı kimdir?",
     options: ["Sezen Aksu", "Sibel Can", "Mustafa Sandal", "Bülent Ersoy"],
     answer: "Mustafa Sandal",
-    media: "/assets/bandira.jpg",
+    media: "/src/assets/bandira.jpg",
   },
   {
     question:
       "Hangi şarkıcımız yine kendisi gibi şarkıcı olan sevgilisinden ayrıldıktan sonra tam evinin karşısındaki apartmanın tamamını kendi posteriyle kaplatmıştır?",
     options: ["Hande Yener", "Hadise", "Gülşen", "Simge"],
     answer: "Hadise",
-    media: "/assets/billboard.jpg",
+    media: "/src/assets/billboard.jpg",
   },
   {
     question: "Antik Roma'da kadınlar parfüm olarak ne kullanıyordu?",
     options: ["Gül Suyu", "Bal", "Gladyatör Teri", "Kan"],
     answer: "Gladyatör Teri",
-    media: "/assets/parfum.jpg",
+    media: "/src/assets/parfum.jpg",
   },
   {
     question: "T-Rex'in yaşayan en yakın akrabası aşağıdakilerden hangisidir?",
     options: ["İnekler", "Tavuklar", "Timsahlar", "Köpekler"],
     answer: "Tavuklar",
-    media: "/assets/trex.jpg",
+    media: "/src/assets/trex.jpg",
   },
   {
     question:
       "Her şeyin olduğu gibi mutluluğun da fobisi varmış. Bu fobiye ne ad verilir?",
     options: ["Çerofobi", "Euphobia", "Felicifobia", "Mutluluk Korkusu"],
     answer: "Çerofobi",
-    media: "/assets/fobi.jpg",
+    media: "/src/assets/fobi.jpg",
   },
 ];
 
@@ -83,26 +83,46 @@ function App() {
   const [quizStarted, setQuizStarted] = useState(false); // Quizin başlatılıp başlatılmadığını kontrol eder
   const [currentQuestion, setCurrentQuestion] = useState(0); // Mevcut sorunun indeksini tutar
   const [correctCount, setCorrectCount] = useState(0); // Doğru cevap sayısını tutar
-  const [timeLeft, setTimeLeft] = useState(30); // Kalan süreyi tutar
+  const [timeLeft, setTimeLeft] = useState(2); // Kalan süreyi tutar
   const [showOptions, setShowOptions] = useState(false); // Seçeneklerin görünürlüğünü kontrol eder
   const [selectedAnswer, setSelectedAnswer] = useState(""); // Kullanıcının seçtiği cevabı tutar
   const [quizEnded, setQuizEnded] = useState(false); // Quizin bitip bitmediğini kontrol eder
   const [userAnswers, setUserAnswers] = useState([]); // Kullanıcının cevaplarını tutar
   const [showNextButton, setShowNextButton] = useState(false); // "Sonraki Soru" butonunun görünürlüğünü kontrol eder
 
-  // Sonraki soruya geçiş işlevi
+  const QUESTION_TIMER = 30;
+
   const handleNextQuestion = useCallback(() => {
-    setShowOptions(false); // Seçenekleri gizle
-    setShowNextButton(false); // "Sonraki Soru" butonunu gizle
-    if (currentQuestion < questions.length - 1) { // Eğer daha fazla soru varsa
-      setCurrentQuestion(currentQuestion + 1); // Sonraki soruya geç
-      setTimeLeft(30); // Süreyi sıfırla
-      setSelectedAnswer(""); // Seçilen cevabı sıfırla
-    } else { // Tüm sorular tamamlandıysa
-      setQuizEnded(true); // Quiz bitti
-      setQuizStarted(false); // Quizin başlangıcını sıfırla
+    setShowOptions(false);
+    setShowNextButton(false);
+  
+    // Eğer cevap seçilmemişse ve bu soru daha önce eklenmemişse boş olarak kaydet
+    if (!selectedAnswer && !quizEnded) {
+      const alreadyAnswered = userAnswers.find(
+        (answers) => answers.question == questions[currentQuestion].question
+      );
+  
+      if (!alreadyAnswered) {
+        setUserAnswers((prevAnswers) => [
+          ...prevAnswers,
+          {
+            question: questions[currentQuestion].question,
+            userAnswer: "Boş Bırakıldı",
+            correctAnswer: questions[currentQuestion].answer,
+          },
+        ]);
+      }
     }
-  }, [currentQuestion]);
+  
+    if (currentQuestion < questions.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+      setTimeLeft(QUESTION_TIMER);
+      setSelectedAnswer("");
+    } else {
+      setQuizEnded(true);
+      setQuizStarted(false);
+    }
+  }, [currentQuestion, selectedAnswer, quizEnded, userAnswers]);
 
   // Timer ve seçenek gösterimi için effect
   useEffect(() => {
@@ -119,7 +139,7 @@ function App() {
           if (prevTime <= 1) { // Süre dolduysa
             clearInterval(timer); // Zamanlayıcıyı temizle
             handleNextQuestion(); // Sonraki soruya geç
-            return 30; // Süreyi sıfırla
+            return QUESTION_TIMER; // Süreyi sıfırla
           }
           return prevTime - 1; // Süreyi 1 azalt
         });
@@ -130,7 +150,7 @@ function App() {
         clearInterval(timer); // Zamanlayıcıyı temizle
       };
     }
-  }, [quizStarted, currentQuestion, handleNextQuestion]);
+  }, [quizStarted, currentQuestion]);
 
   // Kullanıcı cevabını seçme işlevi
   const handleAnswerSelect = (option) => {
@@ -150,12 +170,13 @@ function App() {
     ]);
   };
 
+
   // Quiz başlatma işlevi
   const startQuiz = () => {
     setQuizStarted(true); // Quiz başlat
     setQuizEnded(false); // Quiz bitti bayrağını sıfırla
     setCurrentQuestion(0); // İlk soruya dön
-    setTimeLeft(30); // Süreyi sıfırla
+    setTimeLeft(QUESTION_TIMER); // Süreyi sıfırla
     setCorrectCount(0); // Doğru cevap sayısını sıfırla
     setUserAnswers([]); // Kullanıcı cevaplarını sıfırla
   };
